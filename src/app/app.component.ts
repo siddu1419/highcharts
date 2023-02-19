5f5835435f35298276b8c126b20e5707e810b1e2
siddhu <siddharthadurinti@gmail.com>
2023-02-19 17:37:50 +0530

    initial highcharts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,67 @@
 import { Component } from '@angular/core';
+import * as Highcharts from 'highcharts';
+import { max } from 'rxjs';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+   selector: 'app-root',
+   templateUrl: './app.component.html',
+   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'highchart';
+Highcharts: typeof Highcharts = Highcharts;
+
+No_Of_Questions = [150,100,300 ];
+
+// Find the minimum and maximum values in the array
+minQuestions= Math.min(...this.No_Of_Questions);
+maxQuestions = Math.max(...this.No_Of_Questions);
+
+// Normalizing each value to a range between 0 and 100
+ normalizedValues = this.No_Of_Questions.map(value => (value) / (this.maxQuestions) *100);
+
+
+
+
+
+    chartOptions: any = {
+    chart: {
+      type: 'column'
+    },
+    title: {
+      text: 'My Bar Chart'
+    },
+
+    series: [
+      {
+        name: 'No of Questions',
+        data: this.normalizedValues,
+      },
+      {
+        name: 'Accuracy',
+        data: [20, 30, 40],
+      }
+    ],
+
+    xAxis: {
+      categories: ['Maths', 'Physics', 'Chemistry'],
+
+    },
+
+    yAxis: [{
+      title: {
+        text: 'Primary Axis'
+      },
+      min:0,
+      max:100,
+    },
+    ],
+    plotOptions: {
+      column: {
+        pointWidth: 10
+      }
+    },
+
+
+}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HighchartsChartModule} from 'highcharts-angular';
+import { BarchartComponent } from './barchart/barchart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarchartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HighchartsChartModule
   ],
   providers: [],
   bootstrap: [AppComponent]
